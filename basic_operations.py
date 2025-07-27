597966a4a816455e91e2eca13cd399f8052e5cbb
Ofentse Magongwa <magongwaotsile@gmail.com>
2025-07-27 18:05:16 +0200

    Added code to basic_operations

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10 
+number2 = 5
+sum_result =  10 + 5
+difference_result = 10 - 5
+product_result = 10 * 5
+
+print(f"Addition of 10 and 5 is 15 ")
+print(f"Subtraction of 10 and 5 is 5 ") 
+print(f"Multiplication of 10 and 5 is 50 ")
