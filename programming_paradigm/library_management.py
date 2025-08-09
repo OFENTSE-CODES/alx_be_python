6eaba93b217eed50693daf4b3a16b514ac02e437
Ofentse Magongwa <magongwaotsile@gmail.com>
2025-08-10 00:04:19 +0200

    script for kibrary management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,59 @@
+class Book:
+    """Represents a book with a title, author, and availability status."""
+    
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False  # private attribute
+
+    def check_out(self):
+        """Mark the book as checked out if it's available."""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        """Mark the book as returned (available again)."""
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        """Check if the book is currently available."""
+        return not self._is_checked_out
+
+
+class Library:
+    """Represents a library containing a collection of books."""
+    
+    def __init__(self):
+        self._books = []  # private list to store Book instances
+
+    def add_book(self, book):
+        """Add a Book instance to the library collection."""
+        if isinstance(book, Book):
+            self._books.append(book)
+
+    def check_out_book(self, title):
+        """Find a book by title and check it out if available."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return True
+        return False
+
+    def return_book(self, title):
+        """Find a book by title and return it if it's checked out."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return True
+        return False
+
+    def list_available_books(self):
+        """Print all available books in the library."""
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
