deeffa902cf24bd91a0c1a760ea6d1f27d726bfd
Ofentse Magongwa <magongwaotsile@gmail.com>
2025-08-24 00:39:03 +0200

    script for task 4

--- task_4.sql
+++ task_4.sql
@@ -1,10 +1,10 @@
-SELECT COLUMN_NAME AS 'Field',
-       COLUMN_TYPE AS 'Type',
-       IS_NULLABLE AS 'Null',
-       COLUMN_KEY AS 'Key',
-       COLUMN_DEFAULT AS 'Default',
-       EXTRA AS 'Extra'
+
+SELECT COLUMN_NAME,
+       COLUMN_TYPE,
+       IS_NULLABLE,
+       COLUMN_KEY,
+       COLUMN_DEFAULT,
+       EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books'
-ORDER BY ORDINAL_POSITION;
+  AND TABLE_NAME = 'books';
